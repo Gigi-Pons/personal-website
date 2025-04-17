1e8d4a0be675439009c3da17d444bc24620a4868
Gigi-Pons <grecia.atx@gmail.com>
2025-04-17 13:14:18 -0500

    worked on about section

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const toggle = document.getElementById('menu-toggle');
-    const navbar = document.getElementById('navbar');
+// document.addEventListener('DOMContentLoaded', () => {
+//     const toggle = document.getElementById('menu-toggle');
+//     const navbar = document.getElementById('navbar');
   
-    if (toggle && navbar) {
-      toggle.addEventListener('click', () => {
-        navbar.classList.toggle('active');
-      });
-    }
+//     if (toggle && navbar) {
+//       toggle.addEventListener('click', () => {
+//         navbar.classList.toggle('active');
+//       });
+//     }
+
+//   });
+  
+const toggle = document.getElementById('menu-toggle');
+const navbar = document.getElementById('navbar');
+const navLinks = document.querySelectorAll('#navbar a');
+
+toggle.addEventListener('click', () => {
+  navbar.classList.toggle('active');
+});
+
+// Close mobile menu on link click
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navbar.classList.remove('active');
   });
-  
+});
